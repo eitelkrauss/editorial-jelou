54b7234f275bd7f05080a7e25f25844f75286b4d
eitel <eitelkrauss@protonmail.ch>
2024-04-17 23:43:14 -0500

    persistir state en localStorage con middleware

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,27 @@ import { configureStore } from '@reduxjs/toolkit'
 import myListReducer from '../features/mylist/myListSlice'
 import catalogueReducer from '../features/catalogue/catalogueSlice'
 
+const localStorageMiddleware = ({ getState }) => {
+  return next => action => {
+    const result = next(action);
+    localStorage.setItem('myList', JSON.stringify(getState().myList));
+    return result;
+  };
+};
+
+const repopulateStore = () => {
+  if (localStorage.getItem('myList') !== null) {
+    return {
+      myList: JSON.parse(localStorage.getItem('myList')),
+    };
+  }
+};
+
 export default configureStore({
   reducer: {
     myList: myListReducer,
     catalogue: catalogueReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
+  preloadedState: repopulateStore()
 })
