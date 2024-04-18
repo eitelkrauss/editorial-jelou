2150a26ac9ffca6263723bd12ec2dfae07870205
eitel <eitelkrauss@protonmail.ch>
2024-04-18 08:05:40 -0500

    navbar estilo

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ function App() {
 
   return (
     <>
-      <nav className='sticky top-0 w-full bg-slate-300 h-16 md:hidden'>Editorial jelou</nav>
-      <div className='flex'>
+      <nav className='p-2 flex justify-between sticky top-0 w-full bg-slate-300 h-16 md:hidden'>
+        <h2 className='font-bold text-2xl'>Editorial jelou</h2>
+        <button>Menu</button>
+      </nav>
+      <div className='flex p-12'>
         <MyList />
         <QueryClientProvider client={queryClient}>
           <Catalogue />
