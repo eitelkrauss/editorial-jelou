00dfe99bcd7b00f86af3c790062f36ea1040ff96
eitel <eitelkrauss@protonmail.ch>
2024-04-18 08:06:30 -0500

    eliminar libro de lista onClick

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,12 +1,15 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { removeItem } from "../features/mylist/myListSlice"
 
 export default function MyList () {
     const myList = useSelector((state) => state.myList.value)
+    const dispatch = useDispatch()
 
     return <aside className="h-screen sticky top-0 border-2 p-2 border-slate-400 hidden md:block md:min-w-[300px] md:w-[30%]">
-        <h2 className="text-2xl">Mi Lista</h2>
-        <ul className="flex gap-2 flex-wrap">
-            {Object.entries(myList).map(([_, value]) => <li style={{backgroundImage: `url(${value.cover})`}} className="cursor-pointer bg-contain bg-no-repeat w-10 h-16" key={value.ISBN}></li>)}
+        <h2 className="text-2xl font-semibold">Mi Lista</h2>
+        <ul className="mt-4 flex gap-2 flex-wrap">
+            {Object.entries(myList)
+                .map(([key, value]) => <li onClick={() => dispatch(removeItem(value))} style={{backgroundImage: `url(${value.cover})`}} className="hover:cursor-pointer bg-contain bg-no-repeat w-24 h-36" key={value.ISBN}></li>)}
         </ul>
     </aside>
 }
